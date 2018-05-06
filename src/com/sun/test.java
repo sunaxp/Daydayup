4a737e569afd86ef78fe2e17bf852c82b5116670
sunaxp <sunqianmengx@163.com>
2018-05-06 18:28:57 +0800

    猪2

--- src/com/sun/test.java
+++ src/com/sun/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hahaha");
         System.out.println("wangxinpeishigezhu");
+        System.out.println("猪");
     }
 }
