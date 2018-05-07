ae70068b6b8113c0ac96af04a09c9c33736c91f3
Wxp <283183817@qq.com>
2018-05-07 10:28:15 +0800

    随机数

--- src/com/wxp/test.java
+++ src/com/wxp/test.java
@@ -1,4 +1,46 @@
 package com.wxp;
+import java.util.Random;
 
 public class test {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        int i, j, k;
+        j = rand.nextInt(100) + 1;
+        System.out.println("j : " + j);
+        k = rand.nextInt(100) + 1;
+        System.out.println("k : " + k);
+        i = j + k;
+        System.out.println("j + k : " + i);
+        i = j - k;
+        System.out.println("j - k : " + i);
+        i = k / j;
+        System.out.println("k / j : " + i);
+        i = k * j;
+        System.out.println("k * j : " + i);
+        i = k % j;
+        System.out.println("k % j : " + i);
+        j %= k;
+        System.out.println("j %= k : " + j);
+        float u, v, w;
+        v = rand.nextFloat();
+        System.out.println("v : " + v);
+        w = rand.nextFloat();
+        System.out.println("w : " + w);
+        u = v + w;
+        System.out.println("v + w : " + u);
+        u = v - w;
+        System.out.println("v - w : " + u);
+        u = v * w;
+        System.out.println("v * w : " + u);
+        u = v / w;
+        System.out.println("v / w : " + u);
+        u += v;
+        System.out.println("u += v : " + u);
+        u -= v;
+        System.out.println("u -= v : " + u);
+        u *= v;
+        System.out.println("u *= v : " + u);
+        u /= v;
+        System.out.println("u /= v : " + u);
+    }
 }
