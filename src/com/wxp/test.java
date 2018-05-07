24bab699a346327a7695855f3b8b50462840cd19
Wxp <283183817@qq.com>
2018-05-07 20:59:43 +0800

    hello

--- src/com/wxp/test.java
+++ src/com/wxp/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String[] args) {
         for(char c : "An African Swallow".toCharArray())
             System.out.print(c + " ");
+        System.out.println("hello");
     }
 }
