39d14bb55379c966f3d1a8388abf934ecf1f7c43
sunaxp <sunqianmengx@163.com>
2018-05-07 10:10:12 +0800

    赋值，引用赋值

--- src/com/sun/test.java
+++ src/com/sun/test.java
@@ -1,10 +1,25 @@
 package com.sun;
 
+/**
+ * @author sun
+ * @date 2018.05.07
+ * @content java编程思想三章
+ */
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("hahaha");
-        System.out.println("wangxinpeishigezhu");
-        System.out.println("猪");
+        Tank t1 = new Tank();
+        Tank t2 = new Tank();
+        t1.level = 34;
+        t2.level = 56;
+        System.out.println("1:t1.level:" + t1.level + ",t2.level:" + t2.level);
+        t1 = t2;
+        System.out.println("1:t1.level:" + t1.level + ",t2.level:" + t2.level);
+        t1.level = 78;
+        System.out.println("1:t1.level:" + t1.level + ",t2.level:" + t2.level);
     }
+}
+
+class Tank {
+    int level;
 }
