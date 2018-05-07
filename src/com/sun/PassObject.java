4c8fc1a7e4c590b359d58695f252487bf3fe25da
sunaxp <sunqianmengx@163.com>
2018-05-07 10:21:44 +0800

    方法调用中的别名问题

---
+++ src/com/sun/PassObject.java
@@ -0,0 +1,17 @@
+package com.sun;
+
+public class PassObject {
+    static void f(Letter l){
+        l.c='z';
+    }
+    public static void main(String[] args) {
+        Letter x=new Letter();
+        x.c='a';
+        System.out.println("1:x.c="+" "+x.c);
+        f(x);
+        System.out.println("2:x.c="+" "+x.c);
+    }
+}
+class Letter{
+    char c;
+}
