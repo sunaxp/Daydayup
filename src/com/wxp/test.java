2ac39889206dfdeb03da7d624d584eaef689a107
Wxp <283183817@qq.com>
2018-05-07 20:41:43 +0800

    no message

--- src/com/wxp/test.java
+++ src/com/wxp/test.java
@@ -3,44 +3,7 @@ import java.util.Random;
 
 public class test {
     public static void main(String[] args) {
-        Random rand = new Random(10);
-        int i, j, k;
-        j = rand.nextInt(100) + 1;
-        System.out.println("j : " + j);
-        k = rand.nextInt(100) + 1;
-        System.out.println("k : " + k);
-        i = j + k;
-        System.out.println("j + k : " + i);
-        i = j - k;
-        System.out.println("j - k : " + i);
-        i = k / j;
-        System.out.println("k / j : " + i);
-        i = k * j;
-        System.out.println("k * j : " + i);
-        i = k % j;
-        System.out.println("k % j : " + i);
-        j %= k;
-        System.out.println("j %= k : " + j);
-        float u, v, w;
-        v = rand.nextFloat();
-        System.out.println("v : " + v);
-        w = rand.nextFloat();
-        System.out.println("w : " + w);
-        u = v + w;
-        System.out.println("v + w : " + u);
-        u = v - w;
-        System.out.println("v - w : " + u);
-        u = v * w;
-        System.out.println("v * w : " + u);
-        u = v / w;
-        System.out.println("v / w : " + u);
-        u += v;
-        System.out.println("u += v : " + u);
-        u -= v;
-        System.out.println("u -= v : " + u);
-        u *= v;
-        System.out.println("u *= v : " + u);
-        u /= v;
-        System.out.println("u /= v : " + u);
+        for(char c : "An African Swallow".toCharArray())
+            System.out.print(c + " ");
     }
 }
