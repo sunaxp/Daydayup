19273405b1f409d3738cfd0242314a04ceb61f74
Wxp <283183817@qq.com>
2018-05-08 10:36:20 +0800

    unit4

--- src/com/wxp/test.java
+++ src/com/wxp/test.java
@@ -3,8 +3,25 @@ import java.util.Random;
 
 public class test {
     public static void main(String[] args) {
-        for(char c : "An African Swallow".toCharArray())
-            System.out.print(c + " ");
-        System.out.println("hello");
+        Random rand = new Random(47);
+        for(int i = 0; i < 100; i++) {
+            int c = rand.nextInt(26) + 'a';
+            System.out.print((char)c + ", " + c + ": ");
+            switch (c) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    System.out.println("vowel");
+                    break;
+                case 'y':
+                case 'w':
+                    System.out.println("Sometimes a vowel");
+                    break;
+                default:
+                    System.out.println("constant");
+            }
+        }
     }
 }
